4d79311c08b64e3b4d25dc177697ffc6ea02ad51
Lisa3483 <liz.shtodina@mail.ru>
2023-12-03 20:20:35 +0300

    a

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import math  # +++
 from random import randint
 from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor, QPainterPath
-from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QPushButton
 from PyQt5.QtCore import Qt, QPointF
 from PyQt5 import QtCore, QtGui, QtWidgets
 
@@ -11,8 +11,10 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
-        self.pushButton.clicked.connect(self.push)
+        self.setGeometry(300, 300, 1000, 1000)
+        self.btn = QPushButton('click', self)
+        self.btn.setGeometry(200, 200, 50, 50)
+        self.btn.clicked.connect(self.push)
 
     def push(self):
         qp = QPainter()
